60d2c1f4a0d2b140cf67be320e66104ddb181bc2
Alex Grib <gribkovalexander@gmail.com>
2023-04-23 16:46:11 +0300

    v1

--- app/src/main/java/com/example/smartalarm/AlarmsFragmentViewModel.kt
+++ app/src/main/java/com/example/smartalarm/AlarmsFragmentViewModel.kt
@@ -1,4 +1,74 @@
 package com.example.smartalarm
 
-class AlarmsFragmentViewModel {
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class AlarmsFragmentViewModel : ViewModel() {
+
+    private val currentCalendar = Calendar.getInstance()
+    lateinit var weekCalendarData : WeekCalendarData
+
+    init {
+        updateWeek()
+    }
+
+    fun changeWeek(next: Int) {
+        when (next) {
+            1 -> currentCalendar.add(Calendar.WEEK_OF_YEAR, 1)
+            -1 -> currentCalendar.add(Calendar.WEEK_OF_YEAR, -1)
+        }
+        updateWeek()
+    }
+
+    fun getTodayNumInWeek(): Int {
+        val num = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+        return if (num == 1) 7 else num - 1
+    }
+
+    private fun updateWeek() {
+        val calendar = currentCalendar
+        weekCalendarData = WeekCalendarData()
+
+        while (calendar.get(Calendar.DAY_OF_WEEK) != 2)
+            calendar.add(Calendar.DATE, -1)
+
+        do {
+            weekCalendarData.addDate(
+                calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.MONTH) + 1
+            )
+            calendar.add(Calendar.DATE, 1)
+        } while (calendar.get(Calendar.DAY_OF_WEEK) != 2)
+        calendar.add(Calendar.DATE, -1)
+
+        with(weekCalendarData) {
+            daysList[5].isWeekend = true
+            daysList[6].isWeekend = true
+        }
+
+
+
+        if (
+            Calendar.getInstance().get(Calendar.WEEK_OF_YEAR)
+            ==
+            calendar.get(Calendar.WEEK_OF_YEAR)
+        )
+            weekCalendarData.daysList[getTodayNumInWeek() - 1].today = true
+
+        val firstDayOfWeekMonth = weekCalendarData.daysList[0].getMonthName()
+        val lastDayOfWeekMonth = weekCalendarData.daysList[6].getMonthName()
+
+        with(weekCalendarData.monthList) {
+            if (firstDayOfWeekMonth == lastDayOfWeekMonth) {
+                add("")
+                add(firstDayOfWeekMonth)
+                add("")
+            } else {
+                add(firstDayOfWeekMonth)
+                add("")
+                add(lastDayOfWeekMonth)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/smartalarm/WeekCalendarData.kt
+++ app/src/main/java/com/example/smartalarm/WeekCalendarData.kt
@@ -1,4 +1,46 @@
 package com.example.smartalarm
 
 class WeekCalendarData {
+    val daysList = ArrayList<DateUnit>()
+    val monthList = ArrayList<String>()
+
+    class DateUnit(_dayNumber: Int,
+                   _monthNumber: Int,
+                   _today: Boolean = false,
+                   _isWeekend: Boolean= false,
+                   _isHoliday: Boolean = false
+    ) {
+
+        var dayNumber = _dayNumber
+        var monthNumber = _monthNumber
+        var today: Boolean = _today
+        var isWeekend: Boolean= _isWeekend
+        var isHoliday: Boolean = _isHoliday
+
+        fun getMonthName() : String =
+            when (monthNumber) {
+                1 -> "Январь"
+                2 -> "Февраль"
+                3 -> "Март"
+                4 -> "Апрель"
+                5 -> "Май"
+                6 -> "Июнь"
+                7 -> "Июль"
+                8 -> "Август"
+                9 -> "Сентябрь"
+                10 -> "Октябрь"
+                11 -> "Ноябрь"
+                12 -> "Декабрь"
+                else -> ""
+            }
+    }
+
+    fun addDate(_dayNumber: Int,
+                _monthNumber: Int,
+                _today: Boolean = false,
+                _isWeekend: Boolean= false,
+                _isHoliday: Boolean = false)
+    {
+        daysList.add(DateUnit(_dayNumber, _monthNumber, _today, _isWeekend, _isHoliday))
+    }
 }
