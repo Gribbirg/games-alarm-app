be298f6f58d848f1de75bc9bc5123b86fec20d63
gribk <gribkovalexander@gmail.com>
2023-05-15 23:00:14 +0300

    v2.3 google auth worked

--- app/src/main/java/com/example/smartalarm/data/auth/IAccountService.kt
+++
@@ -1,6 +0,0 @@
-package com.example.smartalarm.data.auth
-
-interface IAccountService {
-    var token: String?
-    var userId: String?
-}
--- app/src/main/java/com/example/smartalarm/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/smartalarm/ui/fragments/ProfileFragment.kt
@@ -1,18 +1,28 @@
 package com.example.smartalarm.ui.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.smartalarm.R
-import com.example.smartalarm.databinding.ActivityMainBinding
 import com.example.smartalarm.databinding.FragmentProfileBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +31,60 @@ class ProfileFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
+        val googleSignInOptions = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignInOptions)
+
+        binding.authButton.setOnClickListener { singIn() }
+
+        binding.exitButton.setOnClickListener {
+            auth.signOut()
+            googleSignInClient.signOut()
+            setViewAccountData()
+        }
+
+        with(auth.currentUser) {
+            if (this != null)
+                setViewAccountData(auth.currentUser?.displayName!!)
+        }
+
         return binding.root
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK)
+                handleResults(GoogleSignIn.getSignedInAccountFromIntent(result.data))
+        }
+
+    private fun singIn() {
+        launcher.launch(googleSignInClient.signInIntent)
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null)
+                setAccountData(account)
+        }
+    }
+
+    private fun setAccountData(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                setViewAccountData(account.displayName!!)
+            } else {
+                Toast.makeText(requireContext(), it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setViewAccountData(userName: String = "Я собака, ты собака") {
+        binding.userNameTextView.text = userName
     }
 }
