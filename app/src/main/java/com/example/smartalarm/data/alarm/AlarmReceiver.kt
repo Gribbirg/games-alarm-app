edd7f67dd6c607df79deb9413fcfe0b2daa8204f
gribk <gribkovalexander@gmail.com>
2023-05-20 01:50:10 +0300

    v2.8.4 More intent

--- app/src/main/java/com/example/smartalarm/data/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/smartalarm/data/alarm/AlarmReceiver.kt
@@ -6,19 +6,28 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.smartalarm.R
-import com.example.smartalarm.ui.activities.CountActivity
+import com.example.smartalarm.ui.activities.GamesActivity
+import java.io.IOException
 
 class AlarmReceiver: BroadcastReceiver() {
+
+    var mediaPlayer: MediaPlayer? = null
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.i("alarm", "Alarm on start!")
         val alarmId = intent?.getLongExtra("alarm id", 0L) ?: return
+        val alarmRisingVolume = intent.getBooleanExtra("alarm rising volume", false)
+        val alarmVibration = intent.getBooleanExtra("alarm vibration", false)
         Log.i("alarm", alarmId.toString())
+        Log.i("alarm", alarmRisingVolume.toString())
+        Log.i("alarm", alarmVibration.toString())
 
-        val intentToActivity = Intent(context, CountActivity::class.java)
+        val intentToActivity = Intent(context, GamesActivity::class.java)
         intentToActivity.putExtra("alarm id", alarmId)
         val pendingIntent = PendingIntent.getActivity(
             context,
@@ -38,5 +47,28 @@ class AlarmReceiver: BroadcastReceiver() {
 
         val notificationManager = NotificationManagerCompat.from(context)
         notificationManager.notify(alarmId.toInt(), notificationBuilder.build())
+        playAudio(context)
+    }
+
+    private fun playAudio(context: Context?) {
+        val audioUrl = "https://www.bensound.com/bensound-music/bensound-ukulele.mp3"
+        mediaPlayer = MediaPlayer()
+        mediaPlayer!!.setAudioStreamType(AudioManager.STREAM_MUSIC)
+
+        try {
+            mediaPlayer!!.setDataSource(audioUrl)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun pauseAudio(context: Context?) {
+        if (mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.stop()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.release()
+        }
     }
 }
