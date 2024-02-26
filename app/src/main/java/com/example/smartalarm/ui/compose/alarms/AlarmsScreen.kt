4d2e644ee7c889a46fa4b447a4aa2bba0afbcd49
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-26 13:23:36 +0300

    Alarms fragment: compose setup

--- app/src/main/java/com/example/smartalarm/ui/compose/alarms/AlarmsScreen.kt
+++ app/src/main/java/com/example/smartalarm/ui/compose/alarms/AlarmsScreen.kt
@@ -1,6 +1,12 @@
 package com.example.smartalarm.ui.compose.alarms
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -8,10 +14,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.Wallpapers
+import com.example.smartalarm.ui.theme.GamesAlarmTheme
 import com.google.android.material.color.MaterialColors
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,8 +31,39 @@ fun AlarmsScreen(
     ) {
     Scaffold(
         topBar = { TopAppBar(title = { Text(text = "РазБудильник") }) },
-        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+    ) {
+        Column (
+            modifier = Modifier
+                .padding(it)
+                .fillMaxWidth()
+                .fillMaxHeight()
+            ,
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        Text(text = it.toString())
+            Text(text = "test")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true, wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE, showSystemUi = true,
+    apiLevel = 33,
+)
+@Composable
+fun AlarmsScreenPreview() {
+    GamesAlarmTheme {
+        AlarmsScreen(state = AlarmsState())
+    }
+}
+
+@Preview(
+    showBackground = true, wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE, showSystemUi = true,
+    apiLevel = 33,
+)
+@Composable
+fun AlarmsScreenDarkPreview() {
+    GamesAlarmTheme (darkTheme = true) {
+        AlarmsScreen(state = AlarmsState())
     }
 }
